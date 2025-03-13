9c9264ff7b08944b16e8048a5d9b84e7d3f5110a
Sarai Jemima Arenivar Hernandez <6796008@clases.edu.sv>
2025-03-12 22:53:18 -0600

    Agregamos las clases Dbcontext

---
+++ AlumnoMVC/Models/AlumDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AlumnoMVC.Models
+{
+    public class AlumDbContext : DbContext
+    {
+        public AlumDbContext(DbContextOptions<AlumDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
---
+++ EmpleadoMVC/Models/EmpleDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmpleadoMVC.Models
+{
+    public class EmpleDbContext : DbContext
+    {
+        public EmpleDbContext(DbContextOptions<EmpleDbContext> options) : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+
+    }
+}
